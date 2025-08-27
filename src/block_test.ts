f312e5d24bd350e5f0013c6fc11b5baaec9046f0
Jacobo Tabernero Rey <jacoborus@gmail.com>
2025-08-27 21:14:06 +0200

    test: unit - getBlockUnitKeys

--- src/block_test.ts
+++ src/block_test.ts
@@ -1,7 +1,8 @@
 import { assertEquals, assertThrows } from "@std/assert";
 
 import { blockSymbol } from "./common.ts";
-import { defineBlock, itemIsBlock } from "./block.ts";
+import { defineBlock, getBlockUnitKeys, itemIsBlock } from "./block.ts";
+import { defineUnit } from "./unit.ts";
 
 Deno.test("block: defineBlock", () => {
   const b1 = defineBlock({
@@ -17,4 +18,36 @@ Deno.test("block: itemIsBlock", () => {
   });
   assertEquals(itemIsBlock(b1), true);
   assertEquals(itemIsBlock({ $: 2 }), false);
+});
+
+Deno.test("block: getBlockUnitKeys", () => {
+  // just public keys
+  const myBlock = defineBlock({
+    a: 1,
+    b: defineBlock({
+      c: 1,
+    }),
+    e: () => 5,
+    f: 5,
+  });
+
+  assertEquals(getBlockUnitKeys(myBlock, true), ["a", "e", "f"]);
+  assertEquals(getBlockUnitKeys(myBlock.b, true), ["c"]);
+
+  function e() {
+    return 5;
+  }
+  e.isPrivate = true as const;
+  // with private keys
+  const myBlock2 = defineBlock({
+    a: 1,
+    b: defineBlock({
+      c: 1,
+    }),
+    e,
+    f: defineUnit(5, { isPrivate: true }),
+  });
+
+  assertEquals(getBlockUnitKeys(myBlock2, false), ["a"]);
+  assertEquals(getBlockUnitKeys(myBlock2.b, false), ["c"]);
 });
