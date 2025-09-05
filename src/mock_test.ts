f005496d49f94900f50822b086c0f86fc75c26e2
Jacobo Tabernero Rey <jacoborus@gmail.com>
2025-09-05 16:44:09 +0200

    test: mockUnit: bound definition

--- src/mock_test.ts
+++ src/mock_test.ts
@@ -1,9 +1,11 @@
 import { assertEquals } from "@std/assert";
 
 import { mockUnit } from "./mock.ts";
+import { defineUnit } from "./unit.ts";
 
-Deno.test("mockUnit: bound", () => {
-  function boundFunc(id: string) {
+Deno.test("mockUnit: bound function", () => {
+  type W = <K extends keyof typeof fakeBlocks>(k: K) => (typeof fakeBlocks)[K];
+  function boundFunc(this: W, id: string) {
     const getUser = this("user.service").getUser;
     return getUser(id);
   }
@@ -33,7 +35,7 @@ Deno.test("mockUnit: bound", () => {
   });
 });
 
-Deno.test("mockUnit: factory", () => {
+Deno.test("mockUnit: factory function", () => {
   function factoryFunc(
     wire: <K extends keyof typeof fakeBlocks>(k: K) => (typeof fakeBlocks)[K],
   ) {
@@ -68,7 +70,7 @@ Deno.test("mockUnit: factory", () => {
   });
 });
 
-Deno.test("mockUnit: async factory", async () => {
+Deno.test("mockUnit: async factory function", async () => {
   async function factoryFunc(
     wire: <K extends keyof typeof fakeBlocks>(k: K) => (typeof fakeBlocks)[K],
   ) {
@@ -95,6 +97,40 @@ Deno.test("mockUnit: async factory", async () => {
 
   const factory = await mockUnit(factoryFunc, fakeBlocks);
   const user = factory("11234");
+
+  if (!user) throw new Error("User not found");
+
+  assertEquals(user, {
+    id: "11234",
+    name: "jacobo",
+    email: "asdfasdf@asdfasdf.asdf",
+    isAdmin: true,
+  });
+});
+
+Deno.test("mockUnit: bound definition", () => {
+  type W = <K extends keyof typeof fakeBlocks>(k: K) => (typeof fakeBlocks)[K];
+  const boundFunc = defineUnit(
+    function boundFunc(this: W, id: string) {
+      const getUser = this("user.service").getUser;
+      return getUser(id);
+    },
+    { isBound: true },
+  );
+
+  const fakeBlocks = {
+    "user.service": {
+      getUser: (id: string) => ({
+        id,
+        name: "jacobo",
+        email: "asdfasdf@asdfasdf.asdf",
+        isAdmin: true,
+      }),
+    },
+  };
+
+  const bound = mockUnit(boundFunc, fakeBlocks);
+  const user = bound("11234");
 
   if (!user) throw new Error("User not found");
 
