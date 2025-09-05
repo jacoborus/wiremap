d92a23a489afb2df16a8f6edace199275709b975
Jacobo Tabernero Rey <jacoborus@gmail.com>
2025-09-05 04:09:03 +0200

    test: mocUnit: async factory function

--- src/mock_test.ts
+++ src/mock_test.ts
@@ -23,7 +23,7 @@ Deno.test("mockUnit: bound", () => {
   const bound = mockUnit(boundFunc, fakeBlocks);
   const user = bound("11234");
 
-  if (!user) throw new Error("Post not found");
+  if (!user) throw new Error("User not found");
 
   assertEquals(user, {
     id: "11234",
@@ -58,7 +58,45 @@ Deno.test("mockUnit: factory", () => {
   const factory = mockUnit(factoryFunc, fakeBlocks);
   const user = factory("11234");
 
-  if (!user) throw new Error("Post not found");
+  if (!user) throw new Error("User not found");
+
+  assertEquals(user, {
+    id: "11234",
+    name: "jacobo",
+    email: "asdfasdf@asdfasdf.asdf",
+    isAdmin: true,
+  });
+});
+
+Deno.test("mockUnit: async factory", async () => {
+  async function factoryFunc(
+    wire: <K extends keyof typeof fakeBlocks>(k: K) => (typeof fakeBlocks)[K],
+  ) {
+    await new Promise((res) => res(true));
+
+    return function (id: string) {
+      const getUser = wire("user.service").getUser;
+      return getUser(id);
+    };
+  }
+  factoryFunc.isFactory = true as const;
+  factoryFunc.isAsyncFactory = true as const;
+
+  const fakeBlocks = {
+    "user.service": {
+      getUser: (id: string) => ({
+        id,
+        name: "jacobo",
+        email: "asdfasdf@asdfasdf.asdf",
+        isAdmin: true,
+      }),
+    },
+  };
+
+  const factory = await mockUnit(factoryFunc, fakeBlocks);
+  const user = factory("11234");
+
+  if (!user) throw new Error("User not found");
 
   assertEquals(user, {
     id: "11234",
